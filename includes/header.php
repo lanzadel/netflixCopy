6ccf6c432ffb65cbac49f5d0efc93ce905b7c620
mchakara <mohamed.chakara@capgemini.com>
2019-12-22 19:38:36 +0100

    Creating the video preview

---
+++ includes/header.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once ("includes/config.php");
+require_once ("includes/classes/PreviewProvider.php");
+require_once ("includes/classes/Entity.php");
+
+
+if(!isset($_SESSION["userLoggedIn"])) {
+    header("Location: register.php");
+}
+
+$userLoggedIn = $_SESSION["userLoggedIn"];
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Reeceflix</title>
+    <link rel="stylesheet" href="assets/style/style.css" type="text/css">
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/06a651c8da.js" crossorigin="anonymous"></script>
+    <script src="assets/js/script.js"></script>
+</head>
+<body>
+
+    <div class="wrapper">
+
+
