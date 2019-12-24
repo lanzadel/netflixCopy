d11c4811426098dc90d9a1451a21723e8199fc67
mchakara <mohamed.chakara@capgemini.com>
2019-12-24 11:57:04 +0100

    creatingTheEntityPage

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once ("includes/header.php");
 
 $preview = new PreviewProvider($con, $userLoggedIn);
 echo $preview->createPreviewVideo(null);
+
 $category = new CategoryContainers($con, $userLoggedIn);
 echo $category->showAllCategories();
 
